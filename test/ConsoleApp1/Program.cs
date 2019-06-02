89e58ef05fc18e0705c61c40c6c30cb569de608a
fren <fenzhicincy@gmail.com>
2019-06-02 15:44:49 -0400

    another change

--- test/ConsoleApp1/Program.cs
+++ test/ConsoleApp1/Program.cs
@@ -10,7 +10,9 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           var person = "Fenzhi";
+
+            Console.WriteLine($"Hello {person}");
         }
     }
 }
